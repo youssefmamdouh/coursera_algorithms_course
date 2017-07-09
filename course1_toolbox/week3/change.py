5c990c8c26371a7cfe7de37dca11026654cbe26d
Youssef Mamdouh <youssef.mamdouh@amplidata.com>
2017-07-09 10:19:11 +0200

    - completed knapsack and change

--- course1_toolbox/week3/change.py
+++ course1_toolbox/week3/change.py
@@ -1,10 +1,19 @@
 # Uses python3
-import sys
+
 
 def get_change(m):
-    #write your code here
-    return m
+    coins = [10, 5, 1]
+    nr_coins_change = 0
+    value = m
+    for coin in coins:
+        if not value:
+            break
+        remainder = value % coin
+        nr_coins_change += (value // coin) if remainder == 0 else (value - remainder) // coin
+        value = remainder
+    return nr_coins_change
+
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
+    m = int(input())
     print(get_change(m))
--- course1_toolbox/week3/fractional_knapsack_1.py
+++ course1_toolbox/week3/fractional_knapsack_1.py
@@ -1,5 +1,4 @@
 # Uses python3
-from collections import OrderedDict
 
 
 def get_optimal_value(capacity, sorted_v_w):
@@ -16,8 +15,13 @@ def get_optimal_value(capacity, sorted_v_w):
 if __name__ == "__main__":
     data = list(map(int, input().split()))
     n, capacity = data[0:2]
-    values = data[2:(2 * n + 2):2]
-    weights = data[3:(2 * n + 2):2]
+    values = []
+    weights = []
+    for _ in range(n):
+        val, weight = list(map(int, input().split()))
+        values.append(val)
+        weights.append(weight)
     v_w = zip(values, weights)
-    opt_value = get_optimal_value(capacity, sorted(v_w, key=lambda t: t[0] // t[1], reverse=True))
-    print("{:.4f}".format(opt_value))
+    print("{:.10f}".format(get_optimal_value(capacity,
+                                             sorted(v_w, key=lambda t: t[0] / t[1],
+                                                    reverse=True))))
